674ae57117dfdfb04372e71c2f840a0d237ea8e1
timeworn <nikolay.voronin93@gmail.com>
2019-03-17 17:29:52 +0800

    fix: sanitise invalid hours and minutes on day and week view ranges

--- projects/angular-calendar/src/modules/month/calendar-month-cell.component.ts
+++ projects/angular-calendar/src/modules/month/calendar-month-cell.component.ts
@@ -23,6 +23,8 @@ import { PlacementArray } from 'positioning';
       let-eventClicked="eventClicked"
       let-tooltipTemplate="tooltipTemplate"
       let-tooltipAppendToBody="tooltipAppendToBody"
+      let-tooltipDelay="tooltipDelay"
+      let-trackByEventId="trackByEventId"
     >
       <div class="cal-cell-top">
         <span class="cal-day-badge" *ngIf="day.badgeTotal > 0">{{
@@ -47,6 +49,7 @@ import { PlacementArray } from 'positioning';
           [tooltipEvent]="event"
           [tooltipTemplate]="tooltipTemplate"
           [tooltipAppendToBody]="tooltipAppendToBody"
+          [tooltipDelay]="tooltipDelay"
           mwlDraggable
           [class.cal-draggable]="event.draggable"
           dragActiveClass="cal-drag-active"
@@ -67,7 +70,9 @@ import { PlacementArray } from 'positioning';
         unhighlightDay: unhighlightDay,
         eventClicked: eventClicked,
         tooltipTemplate: tooltipTemplate,
-        tooltipAppendToBody: tooltipAppendToBody
+        tooltipAppendToBody: tooltipAppendToBody,
+        tooltipDelay: tooltipDelay,
+        trackByEventId: trackByEventId
       }"
     >
     </ng-template>
@@ -100,6 +105,8 @@ export class CalendarMonthCellComponent {
   @Input() customTemplate: TemplateRef<any>;
 
   @Input() tooltipTemplate: TemplateRef<any>;
+
+  @Input() tooltipDelay: number | null;
 
   @Output() highlightDay: EventEmitter<any> = new EventEmitter();
 
